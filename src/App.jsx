efab3de566ce435a00119b6a79266554c7a4e765
Jadline <njerijadline@gmail.com>
2025-03-29 23:14:16 -0500

    final bug fixes and final publishing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AppLayout from "./Pages/AppLayout";
 import UsersList from "./Pages/UserList";
 import Login from "./Pages/Login";
+import ProtectedRoutes from "./Components/ProtectedRoutes";
 
 function App() {
   return (
     <Routes>
       {/* Redirect root path to UsersList */}
-      <Route path="/" element={<Navigate replace to="/userslist" />} />
-
-      {/* Public Route - UsersList (No protection) */}
-      <Route element={<AppLayout />}>
-        <Route path="/userslist" element={<UsersList />} />
-      </Route>
+      <Route path="/" element={<Navigate replace to="/login" />} />
 
       {/* Public Route - Login Page */}
       <Route path="/login" element={<Login />} />
+
+      {/* Protected Routes - Only accessible if logged in */}
+      <Route element={<ProtectedRoutes />}>
+        <Route element={<AppLayout />}>
+          <Route path="/userslist" element={<UsersList />} />
+        </Route>
+      </Route>
     </Routes>
   );
 }
